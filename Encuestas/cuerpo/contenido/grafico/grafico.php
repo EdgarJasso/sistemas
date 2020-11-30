9d2f1b3b6dcaa87dca3e5f71a1dce55d10b70dc4
Edgar Jasso <58747593+EdgarJasso@users.noreply.github.com>
2020-11-30 17:28:20 -0600

    realice 3 graficos

--- Encuestas/cuerpo/contenido/grafico/grafico.php
+++ Encuestas/cuerpo/contenido/grafico/grafico.php
@@ -1,26 +1,157 @@
+<?php include('../../../php/connection.php');?>
 <div class="container-fluid" id="grafico">
     <center>
         <h1>
             Dashboard
         </h1>
     </center>
-    <form class="form-inline">
-        <div class="form-group">
-            <label for="grafico_periodo_periodo">Periodo:</label>
-            <select id="grafico_periodo_periodo" class="form-control">
-                <option value="2020-2">2020-2</option>
-                <?php for ($i=21; $i <=30 ; $i++) { 
-                    echo ' <option value="20'.$i.'-1">20'.$i.'-1</option>';
-                    echo ' <option value="20'.$i.'-2">20'.$i.'-2</option>';
-                }?>
-            </select>
+    <div id="container container_all_graficos">
+
+        <div id="container_all_one">
+
+            <div id="container_all_one_one">
+
+            <form class="text-center">
+                <div class="">
+                    <h4 class="">Encuestas Faltantes</h4>    
+
+                    <label for="grafico_periodo_periodo">Periodo:</label>
+                    <select id="grafico_periodo_periodo" class="">
+                        <option value="2020-2">2020-2</option>
+                        <?php for ($i=21; $i <=30 ; $i++) { 
+                            echo ' <option value="20'.$i.'-1">20'.$i.'-1</option>';
+                            echo ' <option value="20'.$i.'-2">20'.$i.'-2</option>';
+                        }?>
+                    </select>
+                </div>
+            </form>
+            <div class="grafico_periodo">
+                <center><h4 id="grafico_periodo_titulo"></h4></center>
+                <div id="grafico_periodo_contenedor">
+                </div>
+            </div>
+
+            </div>
+
+            <div id="container_all_one_two">
+                <form class="text-center" id="grafico_area_form">
+                    <div class="">
+                    <h4 class="">Encuestas Faltantes del Area:</h4>    
+                        <label for="grafico_area_periodo">Periodo:</label>
+                        <select id="grafico_area_periodo" class="">
+                            <option value="2020-2">2020-2</option>
+                            <?php for ($i=21; $i <=30 ; $i++) { 
+                                echo ' <option value="20'.$i.'-1">20'.$i.'-1</option>';
+                                echo ' <option value="20'.$i.'-2">20'.$i.'-2</option>';
+                            }?>
+                        </select>
+                        <label for="grafico_area_persona">Persona:</label>
+                        <select name="grafico_area_persona" id="grafico_area_nombre" class="">
+                        <?php  
+                        try {
+                            $database = new Connection();
+                            $db = $database->open();
+                            $query="SELECT * FROM ecsnts_area WHERE ecsnts_area.id_area != 1";
+                                foreach ($db->query($query) as $row) {
+                                    echo '<option  value="'.$row['id_area'].'">'.$row['id_area'].' - '.$row['nombre'].'</option>';
+                                }
+                                $db = null;                 
+                        } catch (PDOException $e) {
+                            echo "Error: ".$e->getMessage()." !<br>";
+                        }?> 
+                        </select>
+                    </div>
+                </form>
+                <div class="grafico_area">
+                    <center><h4 id="grafico_area_titulo"></h4></center>
+                    <div id="grafico_area_contenedor">
+                    </div>
+                </div>
+            </div>
         </div>
-    </form>
-    <div class="grafico_periodo">
-        <center><h4 id="grafico_periodo_titulo"></h4></center>
-        <div id="grafico_periodo_contenedor">
+        <div id="container_all_two">
+
+
+            <div id="container_all_one_three">
+
+            <form class="text-center" id="grafico_califica_form">
+                <div class="">
+                <h4 class="">Encuestas Faltantes del usuario:</h4>    
+                    <label for="grafico_califica_periodo">Periodo:</label>
+                    <select id="grafico_califica_periodo" class="">
+                        <option value="2020-2">2020-2</option>
+                        <?php for ($i=21; $i <=30 ; $i++) { 
+                            echo ' <option value="20'.$i.'-1">20'.$i.'-1</option>';
+                            echo ' <option value="20'.$i.'-2">20'.$i.'-2</option>';
+                        }?>
+                    </select>
+                    <label for="grafico_califica_persona">Persona:</label>
+                    <select name="grafico_califica_persona" id="grafico_califica_persona_nombre" class="">
+                    <?php  
+                    try {
+                        $database = new Connection();
+                        $db = $database->open();
+                        $query="SELECT * FROM ecsnts_usuario";
+                            foreach ($db->query($query) as $row) {
+                                echo '<option  value="'.$row['id_usuario'].'">'.$row['id_usuario'].' - '.$row['nombre'].'</option>';
+                            }
+                            $db = null;                 
+                    } catch (PDOException $e) {
+                        echo "Error: ".$e->getMessage()." !<br>";
+                    }?> 
+                    </select>
+                </div>
+            </form>
+            <div class="grafico_califica">
+                <center><h4 id="grafico_califica_titulo"></h4></center>
+                <div id="grafico_califica_contenedor">
+                </div>
+            </div>
+
+            </div>
+
+            <div id="container_all_one_four">
+                <form class="text-center" id="grafico_evaluado_form">
+                    <div class="">
+                        <h4 class="">Evaluaciones Faltantes del usuario:</h4> 
+                        <label for="grafico_evaluado_periodo">Periodo:</label>
+                        <select id="grafico_evaluado_periodo" class="">
+                            <option value="2020-2">2020-2</option>
+                            <?php for ($i=21; $i <=30 ; $i++) { 
+                                echo ' <option value="20'.$i.'-1">20'.$i.'-1</option>';
+                                echo ' <option value="20'.$i.'-2">20'.$i.'-2</option>';
+                            }?>
+                        </select>
+                        <label for="grafico_evaluado_persona">Persona:</label>
+                        <select name="grafico_evaluado_persona" id="grafico_evaluado_persona_nombre" class="">
+                        <?php  
+                        try {
+                            $database = new Connection();
+                            $db = $database->open();
+                            $query="SELECT * FROM ecsnts_usuario";
+                                foreach ($db->query($query) as $row) {
+                                    echo '<option  value="'.$row['id_usuario'].'" >'.$row['id_usuario'].' - '.$row['nombre'].'</option>';
+                                }
+                                $db = null;                 
+                        } catch (PDOException $e) {
+                            echo "Error: ".$e->getMessage()." !<br>";
+                        }?> 
+                        </select>
+                    </div>
+                </form>
+                <div class="grafico_evaluado">
+                <center><h4 id="grafico_evaluado_titulo"></h4></center>
+                <div id="grafico_evaluado_contenedor">
+                </div>
+            </div>
+
+            </div>
+
         </div>
-    </div>
+
+    </div> 
+
+    
 </div>
 
 <script src="../../js/funciones-graficos.js"></script>
--- Encuestas/js/funciones-graficos.js
+++ Encuestas/js/funciones-graficos.js
@@ -1,21 +1,24 @@
 $(document).ready(function () {
+
     grafico_periodo();
     $('#grafico_periodo_periodo').change(grafico_periodo);
-});
 
-function removeData(chart) {
-    chart.data.labels.pop();
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.pop();
-    });
-    chart.update();
-}
+    grafico_area();
+    $('#grafico_area_periodo').change(grafico_area);
+    $('#grafico_area_nombre').change(grafico_area);
+
+    grafico_califica();
+    $('#grafico_califica_periodo').change(grafico_califica);
+    $('#grafico_califica_persona_nombre').change(grafico_califica);
+
+    grafico_evaluado();
+    $('#grafico_evaluado_periodo').change(grafico_evaluado);
+    $('#grafico_evaluado_persona_nombre').change(grafico_evaluado);
+});
 
 function grafico_periodo(){
     $('#grafico_1').remove();
     $('#grafico_periodo_contenedor').append('<canvas id="grafico_1"></canvas>');
-    var titulo = document.getElementById('grafico_periodo_titulo');
-    titulo.innerHTML= '';
     let periodo = document.getElementById('grafico_periodo_periodo').value;
     $.ajax({
         type: "post",
@@ -24,7 +27,6 @@ function grafico_periodo(){
         dataType: "json",
         success: function (response) {
             console.log(response);
-            titulo.innerHTML= periodo;
             var ctx = document.getElementById('grafico_1').getContext('2d');
             var chart = new Chart(ctx, {
                 // The type of chart we want to create
@@ -42,7 +44,17 @@ function grafico_periodo(){
                 },
 
                 // Configuration options go here
-                options: {}
+                options: {
+                    animation: {
+                        tension: {
+                            duration: 1000,
+                            easing: 'linear',
+                            from: 1,
+                            to: 0,
+                            loop: true
+                        }
+                    }
+                }
             });
             chart.update();
         },
@@ -50,4 +62,171 @@ function grafico_periodo(){
             console.log(response);
         }
     });
+}
+
+function grafico_area(){
+    $('#grafico_area').remove();
+   $('#grafico_area_contenedor').append('<canvas id="grafico_area"></canvas>');
+   var titulo = document.getElementById('grafico_area_titulo');
+   titulo.innerHTML= '';
+   let periodo = document.getElementById('grafico_area_periodo').value;
+   let usuario = document.getElementById('grafico_area_nombre').value;
+  // let nombre = document.getElementById('grafico_califica_persona').attr('data-name');
+   $.ajax({
+       type: "post",
+       url:"../../cuerpo/contenido/grafico/area.php",
+       data: "periodo="+periodo + 
+             "&id="+usuario,
+       dataType: "json",
+       success: function (response) {
+           console.log(response);
+           console.log('exito');
+          // titulo.innerHTML= periodo + " " + nombre;
+           var ctx = document.getElementById('grafico_area').getContext('2d');
+           var chart = new Chart(ctx, {
+               // The type of chart we want to create
+               type: 'doughnut',
+
+               // The data for our dataset
+               data: {
+                   labels: ['Faltantes', 'Hechas'],
+                   datasets: [{
+                       label: periodo,
+                       backgroundColor: ['rgb(220,20,3)','rgb(0,192,35)'],
+                       borderColor: ['rgb(251,32,3)','rgb(0,255,46)'],
+                       data: response
+                   }]
+               },
+
+               // Configuration options go here
+               options: {
+                   animation: {
+                       tension: {
+                           duration: 1000,
+                           easing: 'linear',
+                           from: 1,
+                           to: 0,
+                           loop: true
+                       }
+                   }
+               }
+           });
+           chart.update();
+       },
+       error: function(response){
+           console.log(response);
+           console.log('error');
+       }
+   });
+}
+function grafico_califica(){
+     $('#grafico_calificador').remove();
+    $('#grafico_califica_contenedor').append('<canvas id="grafico_calificador"></canvas>');
+    var titulo = document.getElementById('grafico_califica_titulo');
+    titulo.innerHTML= '';
+    let periodo = document.getElementById('grafico_califica_periodo').value;
+    let usuario = document.getElementById('grafico_califica_persona_nombre').value;
+   // let nombre = document.getElementById('grafico_califica_persona').attr('data-name');
+    $.ajax({
+        type: "post",
+        url:"../../cuerpo/contenido/grafico/califica.php",
+        data: "periodo="+periodo + 
+              "&id="+usuario,
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+            console.log('exito');
+           // titulo.innerHTML= periodo + " " + nombre;
+            var ctx = document.getElementById('grafico_calificador').getContext('2d');
+            var chart = new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'doughnut',
+
+                // The data for our dataset
+                data: {
+                    labels: ['Faltantes', 'Hechas'],
+                    datasets: [{
+                        label: periodo,
+                        backgroundColor: ['rgb(220,20,3)','rgb(0,192,35)'],
+                        borderColor: ['rgb(251,32,3)','rgb(0,255,46)'],
+                        data: response
+                    }]
+                },
+
+                // Configuration options go here
+                options: {
+                    animation: {
+                        tension: {
+                            duration: 1000,
+                            easing: 'linear',
+                            from: 1,
+                            to: 0,
+                            loop: true
+                        }
+                    }
+                }
+            });
+            chart.update();
+        },
+        error: function(response){
+            console.log(response);
+            console.log('error');
+        }
+    });
+}
+
+function grafico_evaluado(){
+    $('#grafico_evaluado').remove();
+   $('#grafico_evaluado_contenedor').append('<canvas id="grafico_evaluado"></canvas>');
+   var titulo = document.getElementById('grafico_evaluado_titulo');
+   titulo.innerHTML= '';
+   let periodo = document.getElementById('grafico_evaluado_periodo').value;
+   let usuario = document.getElementById('grafico_evaluado_persona_nombre').value;
+  // let nombre = document.getElementById('grafico_califica_persona').attr('data-name');
+   $.ajax({
+       type: "post",
+       url:"../../cuerpo/contenido/grafico/evaluado.php",
+       data: "periodo="+periodo + 
+             "&id="+usuario,
+       dataType: "json",
+       success: function (response) {
+           console.log(response);
+           console.log('exito');
+          // titulo.innerHTML= periodo + " " + nombre;
+           var ctx = document.getElementById('grafico_evaluado').getContext('2d');
+           var chart = new Chart(ctx, {
+               // The type of chart we want to create
+               type: 'doughnut',
+
+               // The data for our dataset
+               data: {
+                   labels: ['Faltantes', 'Hechas'],
+                   datasets: [{
+                       label: periodo,
+                       backgroundColor: ['rgb(220,20,3)','rgb(0,192,35)'],
+                       borderColor: ['rgb(251,32,3)','rgb(0,255,46)'],
+                       data: response
+                   }]
+               },
+
+               // Configuration options go here
+               options: {
+                   animation: {
+                       tension: {
+                           duration: 1000,
+                           easing: 'linear',
+                           from: 1,
+                           to: 0,
+                           loop: true
+                       }
+                   }
+               }
+           });
+           chart.update();
+       },
+       error: function(response){
+           console.log(response);
+           console.log('error');
+       }
+   });
 }
