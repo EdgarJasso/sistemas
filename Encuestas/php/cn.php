f1327f42ee98b0093c8f4046643f6127f6920214
Edgar Jasso <58747593+EdgarJasso@users.noreply.github.com>
2020-12-04 14:42:20 -0600

    produccion

--- Encuestas/php/cn.php
+++ Encuestas/php/cn.php
@@ -1,17 +1,15 @@
 <?php 
 ob_start();
 function conectar(){
- 
-	$user="root";
-	$db="sistemas";
-	$pass = "";
-	$server="localhost";
-	
-/*
 	$user="epiz_27376931";
 	$db="epiz_27376931_sistemas";
 	$pass = "hj7CPr92l7K";
 	$server="sql302.epizy.com";
+/*
+	$user="root";
+	$db="sistemas";
+	$pass = "";
+	$server="localhost";
 */
 	
 	$con=mysqli_connect($server,$user,$pass,$db) or die("Error".mysqli_connect_error());
